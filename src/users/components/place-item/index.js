db28eadc9b8d2b1b5bb4270d5782e1602cd8c773
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-06-28 18:29:41 +0530

    add custom buttons

--- src/users/components/place-item/index.js
+++ src/users/components/place-item/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 import Card from "../../../shared/components/ui-elements/card";
+import Button from "../../../shared/components/ui-elements/form-elements/button";
+
 import "./place-item.css";
 
-const PlaceItem = ({ image, title, address, description }) => {
+const PlaceItem = ({ image, title, address, description, id }) => {
   return (
     <li className="place-item">
       <Card className="place-item__content">
@@ -16,9 +18,9 @@ const PlaceItem = ({ image, title, address, description }) => {
           <p>{description}</p>
         </div>
         <div className="place-item__actions">
-          <button>View On Map</button>
-          <button>Edit</button>
-          <button>Delete</button>
+          <Button inverse>View On Map</Button>
+          <Button to={`/places/${id}`}>Edit</Button>
+          <Button danger>Delete</Button>
         </div>
       </Card>
     </li>
