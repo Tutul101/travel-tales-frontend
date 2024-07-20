8cd9ede4863020d072bdd5353c23b052b5978333
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-07-20 12:12:40 +0530

    add sign up and connect with backend

--- src/users/pages/auth/index.js
+++ src/users/pages/auth/index.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
 
 import Card from "../../../shared/components/ui-elements/card";
 import Input from "../../../shared/components/ui-elements/form-elements/input";
+import LoadingSpinner from "../../../shared/components/ui-elements/loading-spinner";
+
 import { AuthContext } from "../../../contexts/auth-context";
+
 import "./auth.css";
 import {
   VALIDATOR_EMAIL,
@@ -10,6 +14,7 @@ import {
   VALIDATOR_REQUIRE,
 } from "../../../shared/utils/validators";
 import Button from "../../../shared/components/ui-elements/form-elements/button";
+import ErrorModal from "../../../shared/components/ui-elements/error-modal";
 
 const Auth = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
@@ -20,7 +25,8 @@ const Auth = () => {
   const [emailData, setEmalData] = useState("");
   const [passwordData, setPasswordData] = useState("");
   const [nameData, setNameData] = useState("");
-
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
   const { login } = useContext(AuthContext);
 
   useEffect(() => {
@@ -39,19 +45,53 @@ const Auth = () => {
     }
   }, [isEmailValid, isPasswordValid, isLoginMode]);
 
-  const authSubmitHandler = (e) => {
+  const authSubmitHandler = async (e) => {
     e.preventDefault();
     console.log("Email Data", emailData);
     console.log("Password Data", passwordData);
-    login();
+
+    if (isLoginMode) {
+    } else {
+      try {
+        setIsloading(true);
+        const response = await axios.post(
+          "http://localhost:5000/api/users/signup",
+          {
+            userName: nameData,
+            email: emailData,
+            password: passwordData,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log("response", response);
+        const responseData = await response.data;
+        console.log("responseData", responseData);
+        setIsloading(false);
+        login();
+      } catch (err) {
+        console.log("error", err);
+        setError(err.message || "Someting went wrong here please try again");
+        setIsloading(false);
+      }
+    }
   };
 
   const switchModeHandler = () => {
     setIsLoginMode((prev) => !prev);
   };
+
+  const handleError = () => {
+    setError(null);
+  };
   return (
     <main>
+      {error && <ErrorModal error={error} onClear={handleError} />}
       <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverLay={true} />}
         <h2>Login Required</h2>
         <hr />
         <form onSubmit={authSubmitHandler}>
