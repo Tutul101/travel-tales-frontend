6c251d53cc11cbbf4236d8f86219e7e2059a406e
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-08-02 19:18:41 +0530

    add auto login & logout functionality by storing the token in local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,10 +11,33 @@ import NewPlace from "./places/pages/new-place";
 import UpdatePlace from "./places/pages/update-place";
 import Auth from "./users/pages/auth";
 import { AuthContext } from "./contexts/auth-context";
-function App() {
-  const { isLoggedin, token } = useContext(AuthContext);
 
+let logoutTimer;
+function App() {
+  const { isLoggedin, token, login, logout } = useContext(AuthContext);
+  const [tokenExpire, setTokenExpire] = useState(null);
   console.log("JWT token", token);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    const currentDate = new Date().getTime();
+    const expireDate = storedData && storedData.expiration;
+    setTokenExpire(expireDate);
+    if (storedData && storedData.token && expireDate > currentDate) {
+      login(storedData.userId, storedData.token, expireDate);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpire) {
+      const remainingTime = tokenExpire - new Date().getTime();
+      logoutTimer = setTimeout(() => {
+        logout();
+      }, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token]);
   let routes;
 
   if (isLoggedin) {
--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -11,13 +11,24 @@ export const AuthContext = createContext({
 export const AuthContextProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
-  const login = (uID, jwtToken) => {
+  const login = (uID, jwtToken, expirationDate) => {
     setToken(jwtToken);
     setUserId(uID);
+    const tokenExpierationDate =
+      expirationDate || new Date().getTime() + 1000 * 60 * 60;
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uID,
+        token: jwtToken,
+        expiration: tokenExpierationDate,
+      })
+    );
   };
   const logout = () => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   };
 
   const value = { isLoggedin: !!token, token, userId, login, logout };
